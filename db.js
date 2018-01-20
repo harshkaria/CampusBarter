11539d5a1a1c50ceb331f7b7e6febe0e25e04a33
Swapnil Patil <swap.novateur@gmail.coom>
2018-01-20 04:45:22 -0800

    adding server side

--- db.js
+++ db.js
@@ -4,6 +4,9 @@ var express = require('express')
 var mongoose = require('mongoose')
 var bodyParser = require('body-parser')
 var Comment = require('./model/comments')
+var Item = require('./model/items')
+var User = require('./model/users')
+
 
 var app = express()
 var router = express.Router()
@@ -58,6 +61,56 @@ router.route('/comments')
     });
   });
 
+  router.route('/items')
+    //retrieve all comments from the database
+    .get(function(req, res) {
+      //looks at our Comment Schema
+      Item.find(function(err, items) {
+        if (err)
+          res.send(err);
+        //responds with a json object of our database comments.
+        res.json(items)
+      });
+    })
+    //post new comment to the database
+    .post(function(req, res) {
+      var item = new Item();
+      //body parser lets us use the req.body
+      item.name = req.body.name;
+      item.owner = req.body.owner;
+      item.sold = req.body.sold;
+      item.desc = req.body.desc;
+      item.save(function(err) {
+        if (err)
+          res.send(err);
+        res.json({ message: 'Item successfully added!' });
+      });
+    });
+
+    router.route('/users')
+      //retrieve all comments from the database
+      .get(function(req, res) {
+        //looks at our Comment Schema
+        User.find(function(err, items) {
+          if (err)
+            res.send(err);
+          //responds with a json object of our database comments.
+          res.json(items)
+        });
+      })
+      //post new comment to the database
+      .post(function(req, res) {
+        var user = new User();
+        //body parser lets us use the req.body
+        user.name = req.body.name;
+        item.messages = req.body.messages;
+        item.save(function(err) {
+          if (err)
+            res.send(err);
+          res.json({ message: 'User successfully added!' });
+        });
+      });
+
   //Adding a route to a specific comment based on the database ID
   router.route('/comments/:comment_id')
   //The put method gives us the chance to update our comment based on the ID passed to the route
@@ -89,6 +142,65 @@ router.route('/comments')
      })
    });
 
+   router.route('/items/:item_id')
+   //The put method gives us the chance to update our comment based on the ID passed to the route
+    .put(function(req, res) {
+        Item.findById(req.params.item_id, function(err, item) {
+        if (err)
+          res.send(err);
+        //setting the new author and text to whatever was changed. If nothing was changed
+        // we will not alter the field.
+        (req.body.name) ? item.name = req.body.name : null;
+        (req.body.owner) ? item.owner = req.body.owner : null;
+        (req.body.sold) ? item.sold = req.body.sold : null;
+        (req.body.desc) ? item.desc = req.body.desc : null;
+
+        //save comment
+        item.save(function(err) {
+          if (err)
+            res.send(err);
+          res.json({ message: 'Item has been updated' });
+        });
+      });
+    })
+    //delete method for removing a comment from our database
+    .delete(function(req, res) {
+      //selects the comment by its ID, then removes it.
+      Item.remove({ _id: req.params.item_id }, function(err, item) {
+        if (err)
+          res.send(err);
+        res.json({ message: 'Item has been deleted' })
+      })
+    });
+
+    router.route('/users/:user_id')
+    //The put method gives us the chance to update our comment based on the ID passed to the route
+     .put(function(req, res) {
+         User.findById(req.params.user_id, function(err, user) {
+         if (err)
+           res.send(err);
+         //setting the new author and text to whatever was changed. If nothing was changed
+         // we will not alter the field.
+         (req.body.name) ? user.name = req.body.name : null;
+         (req.body.messages) ? user.messages = req.body.messages : null;
+
+         //save comment
+         user.save(function(err) {
+           if (err)
+             res.send(err);
+           res.json({ message: 'User has been updated' });
+         });
+       });
+     })
+     //delete method for removing a comment from our database
+     .delete(function(req, res) {
+       //selects the comment by its ID, then removes it.
+       User.remove({ _id: req.params.user_id }, function(err, user) {
+         if (err)
+           res.send(err);
+         res.json({ message: 'User has been deleted' })
+       })
+     });
 //Use our router configuration when we call /api
 app.use('/api', router);
 
