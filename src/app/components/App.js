e9ceba112648cd5cbd30bba88b85de42902e9f8b
Swapnil Patil <swap.novateur@gmail.coom>
2018-01-19 23:43:46 -0800

    first commit

---
+++ src/app/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class App extends React.Component {
+  render(){
+      return(
+          <div>
+            <div className="litContainer">
+              <p className="litContainer">A simple React Webpack 2.0 babel kit with bootstrap framework</p>
+            </div>
+          </div>
+      )
+    }
+}
+
+export default App
